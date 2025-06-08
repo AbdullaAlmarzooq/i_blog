cb0496de1847b1621c588b2a8b25d691c718d3a9
Your Name <abdulla.marzooq@hotmail.com>
2025-06-08 19:08:57 +0300

    BlogList added and local database

---
+++ src/BlogList.js
@@ -0,0 +1,20 @@
+const BlogList = ({ blogs, title, handleDelete }) => {
+
+    return ( 
+        <div className="blog-list">
+                {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>{blog.body.substring(0, 100)}...</p>
+                    <p>Written by {blog.author}</p>
+                    <button onClick={() => handleDelete(blog.id)}>Delete blog</button>
+
+                </div>
+            
+            ))}
+        </div>
+    );
+        
+}
+ 
+export default BlogList;
